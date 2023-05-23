c29a8956c1f85d2fc5756d68975caee2a1fba5d5
thamaracarlos <thamara.carlos.oliveira@gmail.com>
2023-05-23 16:42:03 +0100

    task manager

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,30 @@
+class TaskController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'tasks', to: 'task#index'
+  get 'tasks/new', to: 'task#new', as: :new_task
+  post 'tasks', to: 'task#create'
+  get 'tasks/:id', to: 'task#show', as: :task
 end
